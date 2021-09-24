861a7812bd36ce24318722a8ef25f37a579753c6
xiaotiandada <istianlei@qq.com>
2021-09-24 21:26:10 +0800

    Update SliderMore.tsx

--- components/Slider/SliderMore.tsx
+++ components/Slider/SliderMore.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Avatar, Menu, Dropdown } from 'antd'
 import {
   UserOutlined,
@@ -29,83 +29,85 @@ const SliderMore: React.FC<Props> = React.memo(function SliderMore({
   const router = useRouter()
   const { t } = useTranslation('common')
 
-  const MenuJson = [
-    {
-      title: t('contact-us'),
-      item: [
-        {
-          url: 'https://twitter.com/realMetaNetwork',
-          icon: TwitterOutlined,
-          name: 'Twitter'
-        },
-        {
-          url: 'https://t.me/metanetwork',
-          icon: LinkOutlined,
-          name: 'Telegram'
-        },
-        {
-          url: 'https://discord.com/invite/59cXXWCWUT',
-          icon: LinkOutlined,
-          name: 'Discord'
-        },
-        {
-          url: 'https://medium.com/meta-network',
-          icon: MediumOutlined,
-          name: 'Medium'
-        },
-        {
-          url: 'https://github.com/Meta-Network',
-          icon: GithubOutlined,
-          name: 'Github'
-        }
-      ]
-    },
-    {
-      title: t('resource'),
-      item: [
-        {
-          url: 'https://github.com/Meta-Network',
-          icon: null,
-          name: t('version-record')
-        }
-      ]
-    },
-    {
-      title: t('links'),
-      item: [
-        {
-          url: 'https://www.meta.io',
-          icon: null,
-          name: 'Meta.io'
-        },
-        {
-          url: 'https://meta-space.vercel.app',
-          icon: null,
-          name: 'Meta Space'
-        },
-        {
-          url: process.env.NEXT_PUBLIC_META_CMS_URL,
-          icon: null,
-          name: 'Meta CMS'
-        },
-      ]
-    },
-    {
-      title: t('policy'),
-      item: [
-        {
-          url: 'https://meta-network.mttk.net',
-          icon: null,
-          name: t('terms')
-        },
-        {
-          url: 'https://meta-network.mttk.net',
-          icon: null,
-          name: t('privacy-policy')
-        },
-      ]
-    }
-  ]
+  const MenuJson = useMemo(() => {
+    return [
+      {
+        title: t('contact-us'),
+        item: [
+          {
+            url: 'https://twitter.com/realMetaNetwork',
+            icon: TwitterOutlined,
+            name: 'Twitter'
+          },
+          {
+            url: 'https://t.me/metanetwork',
+            icon: LinkOutlined,
+            name: 'Telegram'
+          },
+          {
+            url: 'https://discord.com/invite/59cXXWCWUT',
+            icon: LinkOutlined,
+            name: 'Discord'
+          },
+          {
+            url: 'https://medium.com/meta-network',
+            icon: MediumOutlined,
+            name: 'Medium'
+          },
+          {
+            url: 'https://github.com/Meta-Network',
+            icon: GithubOutlined,
+            name: 'Github'
+          }
+        ]
+      },
+      {
+        title: t('resource'),
+        item: [
+          {
+            url: 'https://github.com/Meta-Network',
+            icon: null,
+            name: t('version-record')
+          }
+        ]
+      },
+      {
+        title: t('links'),
+        item: [
+          {
+            url: 'https://www.meta.io',
+            icon: null,
+            name: 'Meta.io'
+          },
+          {
+            url: 'https://meta-space.vercel.app',
+            icon: null,
+            name: 'Meta Space'
+          },
+          {
+            url: process.env.NEXT_PUBLIC_META_CMS_URL,
+            icon: null,
+            name: 'Meta CMS'
+          },
+        ]
+      },
+      {
+        title: t('policy'),
+        item: [
+          {
+            url: 'https://meta-network.mttk.net',
+            icon: null,
+            name: t('terms')
+          },
+          {
+            url: 'https://meta-network.mttk.net',
+            icon: null,
+            name: t('privacy-policy')
+          },
+        ]
+      }
+    ]
+  }, [])
 
   const menu = (
     <Menu>
