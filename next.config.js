bb7368fc0276fc04f8460fecf8ed184bfeec1204
xiaotiandada <istianlei@qq.com>
2021-10-08 15:09:42 +0800

    feat: :rocket: add sentry

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 const withPlugins = require('next-compose-plugins')
-// const withImages = require('next-images')
 const { i18n } = require('./next-i18next.config')
-// const withReactSvg = require('next-react-svg')
-// const path = require('path')
+
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+const { withSentryConfig } = require('@sentry/nextjs')
 
 const nextConfig = {
+
   reactStrictMode: true,
   i18n,
   webpack: (config, options) => {
@@ -12,5 +17,21 @@ const nextConfig = {
   },
 }
 
-module.exports = withPlugins([
-], nextConfig)
+// Your existing module.exports
+const moduleExports = withPlugins([], nextConfig)
+
+const SentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+}
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions)
