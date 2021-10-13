ccbe462483ffe150a9dca3f123eaaab7b3682949
istianlei <istianlei@qq.com>
2021-10-13 13:02:06 +0800

    Update dev.tsx

--- pages/dev.tsx
+++ pages/dev.tsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/router'
 const key = 'MetaNetworkDEV'
 
 /**
- * dev page test 123
+ * dev page test 1234
  */
 const DEV =  React.memo( function DEV () {
   const router = useRouter()
