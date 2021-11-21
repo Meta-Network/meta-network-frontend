28c52e01656ddcd7319ff3d496318085dcf2c93c
xiaotiandada <istianlei@qq.com>
2021-11-21 12:46:31 +0800

    feat: modify time

--- components/HexGridsCount/Index.tsx
+++ components/HexGridsCount/Index.tsx
@@ -38,9 +38,10 @@ const HexGridsCount: React.FC<Props> = React.memo( function HexGridsCount ({ ran
 
 
   useEffect(() => {
-    const timer = setInterval(fetchHexGridsCountByFilterFn, 4000)
+    const timer = setInterval(fetchHexGridsCountByFilterFn, 10 * 1000)
     const timerShow = setTimeout(() => {
       api.start({ x: 0, opacity: 1 })
+      fetchHexGridsCountByFilterFn()
     }, 3200)
 
     return () => {
